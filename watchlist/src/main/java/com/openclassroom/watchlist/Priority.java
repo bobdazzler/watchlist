15d66aac0b52b464a8957c59eb9470ed612b441c
bobdazzler <Oghorobob93@gmail.com>
2021-08-12 11:10:17 +0100

    Add files via upload
    Am still working on this project but since you requested for something i said i should apply what you want and send it to you

---
+++ watchlist/src/main/java/com/openclassroom/watchlist/Priority.java
@@ -0,0 +1,20 @@
+package com.openclassroom.watchlist;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = PriorityValidator.class)
+
+public @interface Priority {
+String message() default"please enter L,M or H for priority";
+Class<?>[] groups() default{};
+Class<? extends Payload>[]payload() default{};
+
+}
